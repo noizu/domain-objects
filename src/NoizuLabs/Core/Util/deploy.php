525483f5785db3af1fa06136ea8ee4b9ec1f367b
keith <keith.brings@noizu.com>
2014-02-14 11:55:42 +0000

    Stubbing out deploy class

---
+++ src/NoizuLabs/Core/Util/deploy.php
@@ -0,0 +1,27 @@
+<?php
+namespace NoizuLabs\Core\Uril;
+
+class Deploy {
+    public function __construct($configFile) 
+    {
+
+
+    }
+
+    public function deploy() 
+    {
+
+    } 
+
+    public function validate()
+    {
+
+
+    }
+
+    public function promote() 
+    {
+
+
+    }
+}
